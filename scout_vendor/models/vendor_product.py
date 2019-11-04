186aee201fa95c4f68ca81ff9ab19e2246eb3e95
Ishwar <ima.openerp@gmail.com>
2019-11-04 20:05:02 +0530

    [IMP]improved code for scout vendor to set dropship automatic when vendor create product

--- scout_vendor/models/vendor_product.py
+++ scout_vendor/models/vendor_product.py
@@ -9,47 +9,72 @@ class VendorUsers(models.Model):
     
     vendor_user_product = fields.Many2one('res.users',string='Vendor User', default=lambda self: self.env.user)
     
-    is_vendor_product = fields.Boolean('Is Vendor Product')
+    # is_vendor_product = fields.Boolean('Is Vendor Product')
+
+    @api.model_create_multi
+    def create(self,vals_list):
+    	res = super(VendorUsers, self).create(vals_list)
+    	user_id = self.env['res.users'].has_group('scout_vendor.group_vendor_product')
+    	if user_id:
+	    	rule_location = self.env['stock.location.route'].sudo()
+	    	for i in res.route_ids:
+	    		stage_ids = rule_location.search([('name','=','Dropship')])
+	    		if stage_ids:
+	    			res.write({'route_ids':[(6,0,stage_ids.ids)]})
+    	return res
+
+    @api.multi
+    def write(self,vals):
+    	res = super(VendorUsers,self).write(vals)
+    	user_id = self.env['res.users'].has_group('scout_vendor.group_vendor_product')
+    	if user_id:
+    		rule_location = self.env['stock.location.route'].sudo()
+    		for i in res.route_ids:
+    			stage_ids = rule_location.search([('name','=','Dropship')])
+    			if stage_ids:
+    				res.write({'route_ids':[(6,0,stage_ids.id)]})
+    	return res
+    	
     
 
-class VendorSaleOrder(models.Model):
+# class VendorSaleOrder(models.Model):
      
-    _inherit='sale.order'
+#     _inherit='sale.order'
      
-    @api.onchange('pre_country_id')
-    def onchange_code(self):
-        public_categ = self.env['product.public.category'].sudo()
-        if self.order_line:
-            storefront_categ_id = public_categ.get_storefront_category(self.order_line[0].product_id.public_categ_ids)
-            if storefront_categ_id:
-                warehouse_id = storefront_categ_id.parent_id.child_warehouse_id
-                self.warehouse_id = warehouse_id
-                self.webshop_id = warehouse_id.webshop_id
-            else:
-                if self.order_line[0].product_id.is_vendor_product:
-                    self.warehouse_id = self.order_line[0].product_id.public_categ_ids.parent_id.warehouse_id
-                else:
-                    self.warehouse_id = False
-                    self.webshop_id = False
-        else:
-            raise UserError(_('Please Add Orderlines.'))
+#     @api.onchange('pre_country_id')
+#     def onchange_code(self):
+#         public_categ = self.env['product.public.category'].sudo()
+#         if self.order_line:
+#             storefront_categ_id = public_categ.get_storefront_category(self.order_line[0].product_id.public_categ_ids)
+#             if storefront_categ_id:
+#                 warehouse_id = storefront_categ_id.parent_id.child_warehouse_id
+#                 self.warehouse_id = warehouse_id
+#                 self.webshop_id = warehouse_id.webshop_id
+#             else:
+#                 if self.order_line[0].product_id.is_vendor_product:
+#                     self.warehouse_id = self.order_line[0].product_id.public_categ_ids.parent_id.warehouse_id
+#                 else:
+#                     self.warehouse_id = False
+#                     self.webshop_id = False
+#         else:
+#             raise UserError(_('Please Add Orderlines.'))
 
-class VendorCategory(models.Model):
+# class VendorCategory(models.Model):
     
-    _inherit='product.public.category'
+#     _inherit='product.public.category'
     
-    is_vendor_category = fields.Boolean('Is Vendor Category')
+#     is_vendor_category = fields.Boolean('Is Vendor Category')
 
-    def get_storefront_category(self,categ_id):
-        if not categ_id.is_vendor_category:
-            if categ_id.storefront_location_id:
-                return categ_id
+#     def get_storefront_category(self,categ_id):
+#         if not categ_id.is_vendor_category:
+#             if categ_id.storefront_location_id:
+#                 return categ_id
             
-            elif not categ_id.storefront_location_id and categ_id.parent_id:
-                return categ_id.get_storefront_category(categ_id.parent_id)
+#             elif not categ_id.storefront_location_id and categ_id.parent_id:
+#                 return categ_id.get_storefront_category(categ_id.parent_id)
             
-            elif not categ_id.storefront_location_id and not categ_id.parent_id:
-                return False
+#             elif not categ_id.storefront_location_id and not categ_id.parent_id:
+#                 return False
     
 class VendorUsersLogin(models.Model):
 
