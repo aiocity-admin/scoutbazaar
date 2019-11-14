9efb28e680371d05296f84bf736469245c65d8b4
Ishwar <ima.openerp@gmail.com>
2019-11-14 12:45:05 +0530

    [ADD]added block ui on paynow page for vendor and nso

--- scout_stock/static/src/js/int_shipping_method.js
+++ scout_stock/static/src/js/int_shipping_method.js
@@ -3,9 +3,26 @@ odoo.define('scout_stock.international_shipping', function (require) {
     var ajax = require('web.ajax');
     $(document).ready(function(){
     	var int_shippping = $("input.int_shippping")
+    	var int_vendor_shippping = $("input.vendor_int_shippping")
+    	var shipping_length =0
+    	var count = 0
     	
     	
+    	if(int_shippping.length > 0){
+    		shipping_length += int_shippping.length
+	    	$.blockUI({ 
+				message: '<span>Please wait, we are getting best price for you! </span>'
+			});
+    	}
     	
+    	if(int_vendor_shippping.length > 0){
+    		shipping_length += int_vendor_shippping.length
+	    	$.blockUI({ 
+				message: '<span>Please wait, we are getting best price for you! </span>'
+			});
+    	}
+    	
+    	//NSO===========================================
 		$.each( int_shippping, function( key, value ) {
 		var self = this;
 		var delivery_id = $(value).attr('id')
@@ -15,16 +32,53 @@ odoo.define('scout_stock.international_shipping', function (require) {
 			.then(function(vals){
 				if(vals['error_message']){
 					$(self).parent().find('.delivery_price').text(' ')
+					count += 1;
+					if(count == shipping_length){
+						$.unblockUI();
+					}
 //					$(self).parent().find('.delivery_price').text(vals['error_message'])
 				}
 				else{
 					$(self).parent().find('.delivery_price').text(' ')
 					$(self).parent().find('.delivery_price').text(vals['nso_delivery_price'])
+					count += 1;
+					if(count == shipping_length){
+						$.unblockUI();
+					}
 				}
+				
 			})
     	})
     	
+    	//Vendor===========================================
+    	$.each( int_vendor_shippping, function( key, value ) {
+    		var self = this;
+    		var delivery_idi = $(value).attr('id')
+    		var country_codei = $(value).attr('name').split('int_ship_vendor')[1]
+    		
+    		ajax.jsonRpc('/my/calculate/vendor/international_shipping','call',{'vendor_delivery_idi':delivery_idi,'vendor_country_codei':country_codei})
+    		.then(function(vals){
+    			if(vals['error_message']){
+    				$(self).parent().find('.vendor_delivery_price').text(' ')
+    				count += 1;
+					if(count == shipping_length){
+						$.unblockUI();
+					}
+//    				$(self).parent().find('.vendor_delivery_price').text(vals['error_message'])
+    			}
+    			else{
+    				$(self).parent().find('.vendor_delivery_price').text(' ')
+    				$(self).parent().find('.vendor_delivery_price').text(vals['vendor_delivery_pricei'])
+    				count += 1;
+					if(count == shipping_length){
+						$.unblockUI();
+					}
+    			}
+    		})
+    	});
+		
     	
+		//NSO===========================================
     	int_shippping.change(function(e){
     		var self = this;
     		var delivery_id = $(this).attr('id')
@@ -35,7 +89,6 @@ odoo.define('scout_stock.international_shipping', function (require) {
     		ajax.jsonRpc('/calculate/international_shipping','call',{'delivery_id':delivery_id,'country_code':country_code})
     		.then(function(vals){
     			if(vals['error_message']){
-    				console.log("Error--------------")
 //    				$(self).parent().parent().find('.delivery_price').text(' ')
 //    				$(self).parent().find('.delivery_price').text(vals['error_message'])
 //    				location.reload();
--- scout_vendor/static/src/js/int_vendor_shipping_method.js
+++ scout_vendor/static/src/js/int_vendor_shipping_method.js
@@ -3,22 +3,42 @@ odoo.define('scout_vendor.vendor_international_shipping', function (require) {
     var ajax = require('web.ajax');
     $(document).ready(function(){
     	var int_shippping = $("input.vendor_int_shippping")
-    	$.each( int_shippping, function( key, value ) {
-    		var self = this;
-    		var delivery_idi = $(value).attr('id')
-    		var country_codei = $(value).attr('name').split('int_ship_vendor')[1]
-    		ajax.jsonRpc('/my/calculate/vendor/international_shipping','call',{'vendor_delivery_idi':delivery_idi,'vendor_country_codei':country_codei})
-    		.then(function(vals){
-    			if(vals['error_message']){
-    				$(self).parent().find('.vendor_delivery_price').text(' ')
-//    				$(self).parent().find('.vendor_delivery_price').text(vals['error_message'])
-    			}
-    			else{
-    				$(self).parent().find('.vendor_delivery_price').text(' ')
-    				$(self).parent().find('.vendor_delivery_price').text(vals['vendor_delivery_pricei'])
-    			}
-    		})
-    	});
+//    	
+//    	var shipping_length = int_shippping.length
+//    	var count = 0
+//        	
+//    	if(int_shippping.length > 0){
+//	    	$.blockUI({ 
+//				message: '<span>Please wait, we are getting best price for you! </span>'
+//			});
+//    	}
+//    	
+    	
+//    	$.each( int_shippping, function( key, value ) {
+//    		var self = this;
+//    		var delivery_idi = $(value).attr('id')
+//    		var country_codei = $(value).attr('name').split('int_ship_vendor')[1]
+//    		
+//    		ajax.jsonRpc('/my/calculate/vendor/international_shipping','call',{'vendor_delivery_idi':delivery_idi,'vendor_country_codei':country_codei})
+//    		.then(function(vals){
+//    			if(vals['error_message']){
+//    				$(self).parent().find('.vendor_delivery_price').text(' ')
+//    				count += 1;
+//					if(count == shipping_length){
+//						$.unblockUI();
+//					}
+////    				$(self).parent().find('.vendor_delivery_price').text(vals['error_message'])
+//    			}
+//    			else{
+//    				$(self).parent().find('.vendor_delivery_price').text(' ')
+//    				$(self).parent().find('.vendor_delivery_price').text(vals['vendor_delivery_pricei'])
+//    				count += 1;
+//					if(count == shipping_length){
+//						$.unblockUI();
+//					}
+//    			}
+//    		})
+//    	});
     	int_shippping.change(function(e){
     		var self = this;
     		var delivery_id = $(this).attr('id')
