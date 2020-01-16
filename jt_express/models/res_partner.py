2e504092070486880e6d5fde54ad4b9b32cbd3ef
Ishwar <ima.openerp@gmail.com>
2020-01-16 20:07:22 +0530

    [IMP]improved code to change address presentation for PH

--- jt_express/models/res_partner.py
+++ jt_express/models/res_partner.py
@@ -34,6 +34,56 @@ class ResPartner(models.Model):
     district_id = fields.Many2one('res.partner.district',string='City / Municipality')
     town_id = fields.Many2one('res.partner.town',string='Barangay')
     
+    @api.multi
+    def _display_address(self, without_company=False):
+
+        '''
+        The purpose of this function is to build and return an address formatted accordingly to the
+        standards of the country where it belongs.
+
+        :param address: browse record of the res.partner to format
+        :returns: the address formatted in a display that fit its country habits (or the default ones
+            if not country is specified)
+        :rtype: string
+        '''
+        # get the information that will be injected into the display format
+        # get the address format
+        address_format = self._get_address_format()
+        if self.country_id.code == 'PH':
+            address_format = '%(street)s, %(town_id_name)s\n%(district_id_name)s, %(city_id_name)s, %(zip)s\n%(country_name)s'
+            args = {
+                'town_id_name': self.town_id.name or '',
+                'district_id_name': self.district_id.name or '',
+                'city_id_name': self.city_id.name or '',
+                'state_code': self.state_id.code or '',
+                'state_name': self.state_id.name or '',
+                'country_code': self.country_id.code or '',
+                'country_name': self._get_country_name(),
+                'company_name': self.commercial_company_name or '',
+            }
+            for field in self._formatting_address_fields():
+                args[field] = getattr(self, field) or ''
+            if without_company:
+                args['company_name'] = ''
+            elif self.commercial_company_name:
+                address_format = '%(company_name)s\n' + address_format
+            return address_format % args
+        else:
+            address_format = self._get_address_format()
+            args = {
+                'state_code': self.state_id.code or '',
+                'state_name': self.state_id.name or '',
+                'country_code': self.country_id.code or '',
+                'country_name': self._get_country_name(),
+                'company_name': self.commercial_company_name or '',
+            }
+            for field in self._formatting_address_fields():
+                args[field] = getattr(self, field) or ''
+            if without_company:
+                args['company_name'] = ''
+            elif self.commercial_company_name:
+                address_format = '%(company_name)s\n' + address_format
+            return address_format % args
     @api.onchange('city_id')
     def onchange_city(self):
         
