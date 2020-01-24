62ea7d3289c61a834689708ab602f90794085ec4
Ishwar <ima.openerp@gmail.com>
2020-01-24 21:30:24 +0530

    [ADD]added code for report ph address, fix compute button calc as per website and added delivery user on search

--- scout_access_role/models/stock_picking.py
+++ scout_access_role/models/stock_picking.py
@@ -6,11 +6,30 @@ from odoo.addons import decimal_precision as dp
 class StockPicking_LineUser(models.Model):
     _inherit = 'stock.picking'
     
-    delivery_line_user = fields.Many2one('res.users',string='Delivery User',compute='_compute_delivery_line_user')
+    delivery_line_user = fields.Many2one('res.users',string='Delivery User')
     
     @api.multi
-    @api.depends('location_id')
-    def _compute_delivery_line_user(self):
-        if self.location_id and self.location_id.nso_location_id:
-            user_id = self.env['res.users'].sudo().search([('partner_id','=',self.location_id.nso_location_id.id)])
-            self.delivery_line_user = user_id.id
+    def write(self,vals):
+        if vals.get('location_id'):
+            if 'location_id' in vals:
+                location_id = self.env['stock.location'].sudo().search([('id','=',vals.get('location_id'))])
+                if location_id:
+                    if location_id.nso_location_id:
+                        user_id = self.env['res.users'].sudo().search([('partner_id','=',location_id.nso_location_id.id)])
+                        if user_id:
+                            vals.update({'delivery_line_user': user_id.id})
+        return super(StockPicking_LineUser, self).write(vals)
+    
+    @api.model
+    def create(self,vals):
+        if vals.get('location_id'):
+            if 'location_id' in vals:
+                location_id = self.env['stock.location'].sudo().search([('id','=',vals.get('location_id'))])
+                if location_id:
+                    if location_id.nso_location_id:
+                        user_id = self.env['res.users'].sudo().search([('partner_id','=',location_id.nso_location_id.id)])
+                        if user_id:
+                            vals.update({'delivery_line_user': user_id.id})
+        return super(StockPicking_LineUser, self).create(vals)
+    
+
--- scout_stock/models/shipping_address.py
+++ scout_stock/models/shipping_address.py
@@ -95,8 +95,7 @@ class SaleOrderShipping(models.Model):
                         price_total = 0.0
                         for s_line in nso_same_country_location_group[nso_loc]:
                             price_total += s_line.price_total
-                        
-                        temp_price = payment_processing_fee + ((transaction_value/100) * (price_total + res_price.get('price') + handling_price))
+                        temp_price = ((payment_processing_fee + res_price.get('price') + price_total + handling_price) / (1 - transaction_value/100) - (payment_processing_fee + res_price.get('price') + price_total + handling_price))
                         same_delivery_price += (temp_price + res_price.get('price'))
                         delivery_price_split = (temp_price + res_price.get('price'))/len(nso_same_country_location_group[nso_loc])
                         shipping_price_split = res_price.get('price')/len(nso_same_country_location_group[nso_loc])
@@ -136,7 +135,8 @@ class SaleOrderShipping(models.Model):
                             price_total = 0.0
                             for s_line in nso_country_location_group[nso_loc]:
                                 price_total += s_line.price_total
-                            temp_price = payment_processing_fee + ((transaction_value/100) * (price_total + res_price.get('price') + handling_price))
+                            temp_price = ((payment_processing_fee + res_price.get('price') + price_total + handling_price) / (1 - transaction_value/100) - (payment_processing_fee + res_price.get('price') + price_total + handling_price))
+#                             temp_price = payment_processing_fee + ((transaction_value/100) * (price_total + res_price.get('price') + handling_price))
                             delivery_price_split = (temp_price + res_price.get('price'))/len(nso_country_location_group[nso_loc])
                             delivery_price += (temp_price + res_price.get('price'))
                             shipping_price_split = res_price.get('price')/len(nso_country_location_group[nso_loc])
@@ -189,7 +189,8 @@ class SaleOrderShipping(models.Model):
                         price_total = 0.0
                         for s_line in vendor_same_country_based_group[v_cnt]:
                             price_total += s_line.price_total
-                        temp_price = payment_processing_fee + ((transaction_value/100) * (price_total + res_price.get('price') + handling_price))
+                        temp_price = ((payment_processing_fee + res_price.get('price') + price_total + handling_price) / (1 - transaction_value/100) - (payment_processing_fee + res_price.get('price') + price_total + handling_price))
+#                         temp_price = payment_processing_fee + ((transaction_value/100) * (price_total + res_price.get('price') + handling_price))
                         same_delivery_price += (temp_price + res_price.get('price'))
                         delivery_price_split = (temp_price + res_price.get('price'))/len(vendor_same_country_based_group[v_cnt])
                         shipping_price_split = res_price.get('price')/len(vendor_same_country_based_group[v_cnt])
@@ -216,7 +217,8 @@ class SaleOrderShipping(models.Model):
                     price_total = 0.0
                     for s_line in vendor_diff_country_based_group[v_diff_cnt]:
                         price_total += s_line.price_total
-                    temp_price = payment_processing_fee + ((transaction_value/100) * (price_total + res_price.get('price') + handling_price))
+                    temp_price = ((payment_processing_fee + res_price.get('price') + price_total + handling_price) / (1 - transaction_value/100) - (payment_processing_fee + res_price.get('price') + price_total + handling_price))
+#                     temp_price = payment_processing_fee + ((transaction_value/100) * (price_total + res_price.get('price') + handling_price))
                     delivery_price += (temp_price + res_price.get('price'))
                     delivery_price_split = (temp_price + res_price.get('price'))/len(vendor_diff_country_based_group[v_diff_cnt])
                     shipping_price_split = res_price.get('price')/len(vendor_diff_country_based_group[v_diff_cnt])
